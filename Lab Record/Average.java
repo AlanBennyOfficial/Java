5fa98159e5bff5ed762703aad8bcd0a64eccece5
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-07 00:22:24 +0530

    Added files via git

---
+++ Lab Record/Average.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter " + n + " numbers:");
+
+        float[] num = new float[n];
+        float sum = 0;
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextFloat();
+        }
+
+        for (int i = 0; i < n; i++) {
+            sum += num[i];
+        }
+        System.out.println("Average of 5 numbers is: " + sum / n);
+    }
+    
+}
