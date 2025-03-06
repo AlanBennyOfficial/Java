8900cdafcb7dcb64d08040c8fb7296cbf9348e26
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-06 23:34:34 +0530

    Added files via git

---
+++ Lab Record/Distance.java
@@ -0,0 +1,21 @@
+package allen;
+import java.lang.Math;
+import java.util.Scanner;
+public class Distance{
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			
+			System.out.print("Enter x1: ");
+			float x1 = sc.nextFloat();
+			System.out.print("Enter x2: ");
+			float x2 = sc.nextFloat();
+			System.out.print("Enter y1: ");
+			float y1 = sc.nextFloat();
+			System.out.print("Enter y2: ");
+			float y2 = sc.nextFloat();
+			
+			float d = (float) Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+			System.out.println("Distance = " + d );
+		}
+	}
+}
