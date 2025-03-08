0f77e4486531365cb754ba93604fc46028489dde
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-09 00:01:43 +0530

    Added files via git

--- Lab Record/Compound_Interest.java
+++ Lab Record/Compound_Interest.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Compound_Interest {
     public static void main(String[] args) {
-        try(Scanner sc = new Scanner(System.in)){
+        try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter principal amount: ");
             double P = sc.nextDouble();
             System.out.print("Enter rate of interest: ");
             double r = sc.nextDouble();
-            System.out.print("Enter number of times interest applied per time period: ");
-            double n = sc.nextDouble();
-            System.out.print("Enter number of time periods elapsed: ");
-            double t = sc.nextDouble();
+            System.out.print("Enter the number of times interest is compounded per year: ");
+            int n = sc.nextInt();
+            System.out.print("Enter the number of years: ");
+            int t = sc.nextInt();
 
-            double A = P * (Math.pow((1 + r/100), n*t));
-            System.out.println("Compound Interest = " + A);
+            double A = P * Math.pow((1 + r / n), n * t);
+            System.out.println("Compound Amount = "+ A);
         }
     }
 }
