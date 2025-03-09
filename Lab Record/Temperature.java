b49087c1468480402e875f1cbff68678a1799586
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-10 00:20:14 +0530

    Added files via git

--- Lab Record/Temperature.java
+++ Lab Record/Temperature.java
@@ -5,17 +5,17 @@ public class Temperature {
     // convert from celcius to fahrenheit and vice versa
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("What do you want to convert:\n1. Celsius into Farenheight(type 1)\n2. Fraenheight into Celcius(type 2)\nInput:");
+            System.out.println("What do you want to convert?\n1. Celsius into Farenheight(type 1)\n2. Fraenheight into Celcius(type 2)\nInput:");
             int n = sc.nextInt();
 
             if (n==1){
-                System.out.println("Enter temperature in Celsius: ");
+                System.out.print("Enter temperature in Celsius: ");
                 double celsius = sc.nextDouble();
                 double fahrenheit = (celsius * 9 / 5) + 32;
                 System.out.println(celsius + "°C is " + fahrenheit + "°F");
             }
             else {
-                System.out.println("Enter temperature in Fahrenheit: ");
+                System.out.print("Enter temperature in Fahrenheit: ");
                 double fahrenheit = sc.nextDouble();
                 double celsius = (fahrenheit - 32) * 5 / 9;
                 System.out.println(fahrenheit + "°F is " + celsius + "°C");
