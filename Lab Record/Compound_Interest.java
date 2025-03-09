402eeff1d3adbd8457a2c13d24d3e4940d6dbf6b
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-09 15:09:24 +0530

    Added files via git

--- Lab Record/Compound_Interest.java
+++ Lab Record/Compound_Interest.java
@@ -12,7 +12,7 @@ public class Compound_Interest {
             System.out.print("Enter the number of years: ");
             int t = sc.nextInt();
 
-            double A = P * Math.pow(1 + (r/100) / n, n * t);
+            double A = P * Math.pow(1 + (r/100) / n, n * t); 
             System.out.println("Compound Amount = "+ A);
         }
     }
