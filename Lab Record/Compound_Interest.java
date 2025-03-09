721a36d57580b27de91ac4260dee13bec4a09f92
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-09 15:07:02 +0530

    Added files via git

--- Lab Record/Compound_Interest.java
+++ Lab Record/Compound_Interest.java
@@ -1,20 +1,18 @@
 // Write a Java program to compute compound interest using the formula A = P(1 + r/n)^(nt).
-
 import java.util.Scanner;
-
 public class Compound_Interest {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter principal amount: ");
             double P = sc.nextDouble();
-            System.out.print("Enter rate of interest: ");
+            System.out.print("Enter annual interest rate (as a decimal): ");
             double r = sc.nextDouble();
             System.out.print("Enter the number of times interest is compounded per year: ");
             int n = sc.nextInt();
             System.out.print("Enter the number of years: ");
             int t = sc.nextInt();
 
-            double A = P * Math.pow((1 + r / n), n * t);
+            double A = P * (Math.pow((1 + r / n), (n * t)));
             System.out.println("Compound Amount = "+ A);
         }
     }
