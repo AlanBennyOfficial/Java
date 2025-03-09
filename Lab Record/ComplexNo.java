f4059571eba35cbc0d8cd55dac6a263aa558e769
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-09 20:43:30 +0530

    Added files via git

--- Lab Record/ComplexNo.java
+++ Lab Record/ComplexNo.java
@@ -2,30 +2,6 @@
 
 import java.util.Scanner;
 
-public class Ops_Complex_Num{
-    public static void main(String[] args){
-        try(Scanner sc = new Scanner(System.in)){
-            System.out.println("Enter the real and imaginary parts of the first complex number: ");
-            double real1 = sc.nextDouble();
-            double img1 = sc.nextDouble();
-            
-            System.out.println("Enter the real and imaginary parts of the second complex number: ");
-            double real2 = sc.nextDouble();
-            double img2 = sc.nextDouble();
-            
-            System.out.println("The sum of the two complex numbers is: " + (real1 + real2) + " + " + (img1 + img2) + "i");
-            System.out.println("The difference of the two complex numbers is: " + (real1 - real2) + " + " + (img1 - img2) + "i");
-            System.out.println("The product of the two complex numbers is: " + (real1 * real2 - img1 * img2) + " + " + (real1 * img2 + img1 * real2) + "i");
-            System.out.println("The division of the two complex numbers is: " + (real1 * real2 + img1 * img2) / (real2 * real2 + img2 * img2) + " + " + (img1 * real2 - real1 * img2) / (real2 * real2 + img2 * img2) + "i");
-        }
-    }
-    
-}
-
-// Another method
-/*
-import java.util.Scanner;
-
 class Complex
 {
 	private int real;
@@ -107,4 +83,3 @@ public class ComplexNo
 		}
 	
 }
- */
