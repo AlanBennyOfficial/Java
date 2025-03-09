2f430cb791a5153dd862c5f47d3e3eb1d09e94a5
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-09 18:08:41 +0530

    Added files via git

--- Lab Record/Person.java
+++ Lab Record/Person.java
@@ -1,4 +1,4 @@
-// 9Write a Java program to define a class Person with attributes name and age, and display the details of a person.
+// Write a Java program to define a class Person with attributes name and age, and display the details of a person.
 public class Person {
     
 }
