0c79eabb5054c24a7e8a4a078a4ebf0161166233
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-09 20:44:56 +0530

    Added files via git

--- Lab Record/ComplexNo.java
+++ Lab Record/ComplexNo.java
@@ -50,36 +50,36 @@ public class ComplexNo
 {
 	public static void main(String[]args)
 	{
-		Scanner sc = new Scanner(System.in);
-		Complex c1 = new Complex();
-		Complex c2 = new Complex(10);
-		Complex c3 = new Complex(10,20);
-		c1.display();
-		c2.display();
-		c3.display();
-		
-		System.out.print("Enter one value for real and imaginary part:");
-		int x1 = sc.nextInt();
-		Complex c_1 = new Complex(x1);
-		
-		System.out.print("Enter two value for real abd Imaginary Part:");
-		int x2 = sc.nextInt();
-		int y2 = sc.nextInt();
-		Complex c_2 = new Complex(x2,y2);
-		
-		Complex c_3 = new Complex();
-		c_3.add(c_1, c_2);
-		
-		Complex c_4 = new Complex();
-		c_4 = c_4.sub(c_1,c_2);
-		
-		Complex c_5 = new Complex();
-		c_5.mul(c_1, c_2);
-		
-		c_3.display();
-		c_4.display();
-		c_5.display();
-		sc.close();
+            try (Scanner sc = new Scanner(System.in)) {
+                Complex c1 = new Complex();
+                Complex c2 = new Complex(10);
+                Complex c3 = new Complex(10,20);
+                c1.display();
+                c2.display();
+                c3.display();
+                
+                System.out.print("Enter one value for real and imaginary part:");
+                int x1 = sc.nextInt();
+                Complex c_1 = new Complex(x1);
+                
+                System.out.print("Enter two value for real abd Imaginary Part:");
+                int x2 = sc.nextInt();
+                int y2 = sc.nextInt();
+                Complex c_2 = new Complex(x2,y2);
+                
+                Complex c_3 = new Complex();
+                c_3.add(c_1, c_2);
+                
+                Complex c_4 = new Complex();
+                c_4 = c_4.sub(c_1,c_2);
+                
+                Complex c_5 = new Complex();
+                c_5.mul(c_1, c_2);
+                
+                c_3.display();
+                c_4.display();
+                c_5.display();
+            }
 		}
 	
 }
