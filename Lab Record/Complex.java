da6a8954236ef82778faf47e21a34211ccea056e
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-10 20:55:07 +0530

    Added files via git

--- Lab Record/Complex.java
+++ Lab Record/Complex.java
@@ -1,19 +1,16 @@
-// From: Anish
+// another method
 import java.util.Scanner;  
 public class Complex {  
     public static void main(String[] args) {  
-        Scanner sc = new Scanner(System.in);  
-        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();  
-
-        System.out.println((a + c) + " + " + (b + d) + "i");  
-        System.out.println((a - c) + " + " + (b - d) + "i");  
-        System.out.println((a * c - b * d) + " + " + (a * d + b * c) + "i");  
-
-        double denominator = c * c + d * d;  
-        double realPart = (double) (a * c + b * d) / denominator;  
-        double imagPart = (double) (b * c - a * d) / denominator;  
-        System.out.println(realPart + " + " + imagPart + "i");  
-
-        sc.close();
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+            System.out.println((a + c) + " + " + (b + d) + "i");
+            System.out.println((a - c) + " + " + (b - d) + "i");
+            System.out.println((a * c - b * d) + " + " + (a * d + b * c) + "i");
+            double denominator = c * c + d * d;
+            double realPart = (double) (a * c + b * d) / denominator;
+            double imagPart = (double) (b * c - a * d) / denominator;
+            System.out.println(realPart + " + " + imagPart + "i");
+        }
     }  
 }
